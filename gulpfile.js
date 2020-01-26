97411aa7b11621da412f7121c86b689cdd142302
unknown <josuedaniel831@yahoo.com>
2020-01-25 22:15:15 -0700

    html and scss automatically refreshes in the browser using gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ function style() {
 	//1. where is my scss file
 	return gulp.src('./scss/**/*.scss')
 	//2. pass that file through sass compiler
-	.pipe(sass())
+	.pipe(sass().on('error', sass.logError))
 	//3. where do I save the compiled css?
 	.pipe(gulp.dest('./css'))
 
